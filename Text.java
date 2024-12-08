81d5456233e4a13dacc72885673bb6ac737bcf0e
Cake2Rock <maxslisarenko8@gmail.com>
2024-12-08 19:21:35 +0200

    Added text

---
+++ Text.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Text {
+    private List<Sentence> sentences;
+
+    public Text(String text) {
+        sentences = new ArrayList<>();
+        String[] splitSentences = text.split("(?<=[.!?])\\s+");
+
+        for (String sentence : splitSentences) {
+            sentences.add(new Sentence(sentence));
+        }
+    }
+
+    public List<Sentence> getSentences() {
+        return sentences;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (Sentence sentence : sentences) {
+            builder.append(sentence).append(" ");
+        }
+        return builder.toString().trim();
+    }
+}
